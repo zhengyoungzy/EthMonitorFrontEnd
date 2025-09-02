fcabfb7fca48a0c7de08449d1b44347185273b8c
Lucian Zheng <zheng.young.zy@gmail.com>
2025-09-03 09:34:02 +1000

    feat: set port

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        port: import.meta.env.PORT,
+        strictPort: true
+    }
 })
