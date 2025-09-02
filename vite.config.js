7e3fe0082cdc557c11ef71a447bd4ea3034f3884
Lucian Zheng <zheng.young.zy@gmail.com>
2025-09-03 09:37:48 +1000

    fix: add port

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: import.meta.env.PORT,
-        strictPort: true
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        plugins: [react()],
+        server: {
+            port: parseInt(env.PORT) || 5173
+        }
     }
 })
